23e8ac15535af2d2d4c5e6e428814adf71be5c95
samuraimasa <masanori.sano0327@gmail.com>
2021-04-02 00:11:08 +0900

    typo

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { ConfigService } from '@nestjs/config'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  await app.listen(configService.get('PORT') || 3000);
+  await app.listen(configService.get('port') || 3000);
 }
 bootstrap();
